c105c766e0dc4885320c2052845285af121eac4a
Ankit Raj <ankitsingh17072001@gmail.com>
2024-02-16 11:19:06 +0530

    [Ankit] Update: exploring more functionality of alter command

--- DDL Commands.sql
+++ DDL Commands.sql
@@ -26,3 +26,20 @@ alter table empTable alter column phone varchar(20);   -- alter column is used t
 exec sp_columns 'empTable';    -- sp_columns is used to describe the table structure.
 
 truncate table empTable;     -- it is used to delete the complete data of the data without affecting the table structure.
+
+--ALTER TABLE IS USED TO MODIFY THE STRUCTURE OF THE EXISTING TABLE .
+--ADD OPERATION 
+	alter table empTable add age int;   --adding a new column to the table.
+
+--DROP OPERATION 
+	alter table empTable drop column age;
+
+--MODIFYING COLUMN DATATYPE
+	alter table empTable alter column phone int;
+
+--ADD CONSTRAINT
+	ALTER TABLE empTable ADD CONSTRAINT PrimaryKey PRIMARY KEY (empId);
+
+--DROP PRIMART KEY
+	ALTER TABLE empTable DROP CONSTRAINT PrimaryKey;
+ 
